25d14c7c7e9dfcd7bfaa9bfe0bd36917048f8531
lnkkerst <1593510417@qq.com>
2022-02-09 13:11:41 +0800

    xprofile zsh terminator

--- config/.zsh/mycfg.zsh
+++ config/.zsh/mycfg.zsh
@@ -3,6 +3,9 @@ alias lla='ls -alh'
 alias l='ls'
 alias la='ls -a'
 alias vi='vim'
-alias qq='bwrap --dev-bind / / --tmpfs /usr/share/fonts/windows-zh_cn/ /opt/apps/com.qq.im.deepin/files/run.sh'
+alias qq='bwrap --dev-bind / / --tmpfs /usr/share/fonts/windows-zh_cn /opt/apps/com.qq.im.deepin/files/run.sh'
+alias tim='bwrap --dev-bind / / --tmpfs /usr/share/fonts/windows-zh_cn /opt/apps/com.qq.office.deepin/files/run.sh'
+alias true-rm='/bin/rm'
+alias rm='trash-put'
 
 source /usr/share/nvm/init-nvm.sh
