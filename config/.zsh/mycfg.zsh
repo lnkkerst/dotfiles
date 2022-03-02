463e2ed96f9e6f4d7010c542d7bf71a9afee88cb
lnkkerst <1593510417@qq.com>
2022-03-02 21:50:42 +0800

    zsh i3 mpd ncmpcpp xsettingsd

--- config/.zsh/mycfg.zsh
+++ config/.zsh/mycfg.zsh
@@ -1,3 +1,5 @@
+export EDITOR='vim'
+
 alias ll='ls -lh'
 alias lla='ls -alh'
 alias l='ls'
@@ -7,5 +9,12 @@ alias qq='bwrap --dev-bind / / --tmpfs /usr/share/fonts/windows-zh_cn /opt/apps/
 alias tim='bwrap --dev-bind / / --tmpfs /usr/share/fonts/windows-zh_cn /opt/apps/com.qq.office.deepin/files/run.sh'
 alias true-rm='/bin/rm'
 alias rm='trash-put'
+alias pc='proxychains'
 
 source /usr/share/nvm/init-nvm.sh
+
+# yarn 
+#export PATH=$HOME/.yarn/bin:$PATH
+
+# custom bin
+#export PATH=$HOME/.local/bin:$PATH
