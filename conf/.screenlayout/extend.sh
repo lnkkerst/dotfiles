65223db5f8e534581365372a5502c6dd1c707686
lnkkerst <1593510417@qq.com>
2022-04-02 16:10:48 +0800

    screenlayout

---
+++ conf/.screenlayout/extend.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --primary --mode 2560x1440 --pos 1440x0 --rotate normal --output eDP --mode 1440x900 --pos 0x540 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
---
+++ conf/.screenlayout/single.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --off --output eDP --primary --mode 2560x1600 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
