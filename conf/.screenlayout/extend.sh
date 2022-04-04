3469a25c3c539673eff3ed256f2847b250b092bd
lnkkerst <1593510417@qq.com>
2022-04-04 13:15:25 +0800

    xinitrc

--- conf/.screenlayout/extend.sh
+++ conf/.screenlayout/extend.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-A-0 --primary --mode 2560x1440 --pos 1440x0 --rotate normal --output eDP --mode 1440x900 --pos 0x540 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
+xrandr --output HDMI-A-0 --off --output eDP --mode 1440x900 --pos 0x540 --rotate normal --output DisplayPort-0 --primary --mode 2560x1440 --rate 120 --pos 1440x0 --rotate normal --output DisplayPort-1 --off
