c2b11277961e920b7cbea740b162d066d847e9bc
lnkkerst <lnkkerst04@gmail.com>
2023-07-10 01:29:06 +0800

    feat: update fish, hypr, xinit

---
+++ fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.5.5
+set -g VIRTUALFISH_PYTHON_EXEC /home/lnk/.local/pipx/venvs/virtualfish/bin/python
+source /home/lnk/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
